30e21b99d6eaec1cb8a9f397fb10bb0d774220a2
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2024-10-29 20:58:53 +0800

    Create 3. Exemple des carnivores.pl

---
+++ Prolog - Programmation par l'exemple/Chapitre 1/3. Exemple des carnivores.pl
@@ -0,0 +1,52 @@
+/* 3. Exemple des carnivores
+
+Écrire les clauses Prolog correspondant au fait que les animaux sont herbivores 
+ou carnivores, l'antilope est un herbivore, le lion est féroce et d'ailleurs, 
+tous les animaux féroces sont des carnivores. Les carnivores mangent de la 
+viande et des herbivores, lesquels mangent de l'herbe. Tous boivent de l'eau.
+
+Qui consomme quoi ? Développer l'arbre de recherche.
+
+[J. L. Laurière Intelligence artificielle, Tomes I et II, Eyrolles 1986, 1987].
+
+*/
+
+animal(X) :-
+    herbivore(X).
+
+animal(X) :-
+    carnivore(X).
+
+herbivore(antilope).
+
+feroce(lion).
+
+carnivore(X) :-
+    feroce(X).
+
+mange(X, viande) :-
+    carnivore(X).
+
+mange(X, Y) :-
+    carnivore(X),
+    herbivore(Y).
+
+mange(X, herbe) :-
+    herbivore(X).
+
+boit(X, eau) :-
+    animal(X).
+
+consomme(X, Y) :-
+    mange(X, Y).
+
+consomme(X, Y) :-
+    boit(X, Y).
+
+/** <examples>
+
+?- consomme(X, Y).
+X = lion,
+Y = viande .
+
+*/
