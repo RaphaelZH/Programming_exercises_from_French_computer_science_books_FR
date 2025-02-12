ad89b1ff42e88c69f25816f9d0c24915a2d2fc56
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-02-11 22:47:18 -0500

    9. Combinaisons

---
+++ Prolog - Programmation par l'exemple/Chapitre 1/9. Combinaisons.pl
@@ -0,0 +1,11 @@
+/* 9. Combinaisons
+
+Faire une programmation de complexité linéaire pour le coefficient C_{n}^k. 
+Dessiner l’arbre de résolution de comb(5, 3 X). par la méthode naïve, puis par 
+celle-ci en comptant le nombre de nœuds de chacune.
+
+*/
+
+
+
+    
